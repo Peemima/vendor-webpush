1f55f8ac8a5ed0aaa7e0830e2df660eaeea477d2
Peemima <96958647+Peemima@users.noreply.github.com>
2025-02-15 19:28:37 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,14 @@
 $composerVersion = shell_exec('composer --version');
 echo 'Composer Version: ' . $composerVersion . "<br>";
 
+// Check if autoloader exists
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require __DIR__ . '/vendor/autoload.php';
+    echo 'Autoloader found and loaded.<br>';
+} else {
+    echo 'Autoloader not found.<br>';
+}
+
 // Check if WebPush package is installed
 if (class_exists('Minishlink\WebPush\WebPush')) {
     echo 'WebPush package is installed.<br>';
@@ -10,10 +18,7 @@ if (class_exists('Minishlink\WebPush\WebPush')) {
     echo 'WebPush package is NOT installed.<br>';
 }
 
-// You can also check other dependencies similarly, for example:
-// if (class_exists('Some\Other\Class')) {
-//     echo 'Some other package is installed.<br>';
-// } else {
-//     echo 'Some other package is NOT installed.<br>';
-// }
+// Check installed packages
+$installedPackages = shell_exec('composer show');
+echo 'Installed Packages:<br>' . nl2br($installedPackages);
 ?>
