b731382866713d323010972ee2e15df61b217c0f
Peemima <96958647+Peemima@users.noreply.github.com>
2025-02-15 19:17:28 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-echo 'Composer Version: ' . shell_exec('composer --version');
+// Check Composer version
+$composerVersion = shell_exec('composer --version');
+echo 'Composer Version: ' . $composerVersion . "<br>";
+
+// Check if a specific package is installed
+if (class_exists('Minishlink\WebPush\WebPush')) {
+    echo 'WebPush package is installed.<br>';
+} else {
+    echo 'WebPush package is NOT installed.<br>';
+}
 ?>
