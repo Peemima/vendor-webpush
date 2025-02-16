3ce4f4907a9992f469da0405ff399f88d42ff5bf
Peemima <96958647+Peemima@users.noreply.github.com>
2025-02-15 19:22:42 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,17 @@
 $composerVersion = shell_exec('composer --version');
 echo 'Composer Version: ' . $composerVersion . "<br>";
 
-// Check if a specific package is installed
+// Check if WebPush package is installed
 if (class_exists('Minishlink\WebPush\WebPush')) {
     echo 'WebPush package is installed.<br>';
 } else {
     echo 'WebPush package is NOT installed.<br>';
 }
+
+// You can also check other dependencies similarly, for example:
+// if (class_exists('Some\Other\Class')) {
+//     echo 'Some other package is installed.<br>';
+// } else {
+//     echo 'Some other package is NOT installed.<br>';
+// }
 ?>
